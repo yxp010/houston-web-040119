291635f65a118d9c2037bc68dae2a54c4a99329d
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-04-22 11:33:35 -0500

    sinatra-MVC

--- 14-Sinatra-and-MVC/app/controllers/application_controller.rb
+++ 14-Sinatra-and-MVC/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < Sinatra::Base
     set :views, "app/views"
     set :method_override, true
 
+    #home page
     get '/' do
         @hello = "040119"
         erb :home
@@ -15,26 +16,31 @@ class ApplicationController < Sinatra::Base
         erb :index
     end
 
+    #new book form
     get '/books/new' do
         erb :new
     end
 
+    #display a book info
     get '/books/:id' do
         @book = Book.find(params[:id])
         erb :'show'
     end
 
+    #create a book
     post '/books' do
         # binding.pry
         Book.create(params)
         redirect '/books'
     end
 
+    #edit a book form
     get '/books/:id/edit' do
         @book = Book.find(params[:id])
         erb :edit
     end
 
+    #edit a book
     patch '/books/:id' do
         book = Book.find(params[:id])
         # binding.pry
@@ -42,13 +48,11 @@ class ApplicationController < Sinatra::Base
         redirect "/books/#{params[:id]}"
     end
 
+    #delete a book
     delete '/books/:id' do
         book = Book.find(params[:id])
         book.destroy 
         redirect "/books"
     end
-
-    
-
 
 end
